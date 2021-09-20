d8b053b51452f61eae72e8978d93ac0ead2d266d
Ahmed Omar <misteradnan1@gmail.com>
2021-09-20 17:17:16 +0600

    #1 String

---
+++ CODE/Variable/string.py
@@ -0,0 +1,14 @@
+greetings = "Hello World!"
+
+print(greetings)
+
+greetings = greetings.replace("e","i")
+
+print(greetings)
+
+print(len(greetings))
+
+print("length of greetings is: " + str(len(greetings)))
+
+
+print(dir(greetings))
