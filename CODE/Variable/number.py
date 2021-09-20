5e40b8336e94fb296c4ed8b289c36e309d77a936
Ahmed Omar <misteradnan1@gmail.com>
2021-09-20 17:17:07 +0600

    #1 Numbers

---
+++ CODE/Variable/number.py
@@ -0,0 +1,33 @@
+a = 1
+b = 2
+print(a+b)
+
+a2 = "1"
+b2 = "2"
+print(a2+b2)
+
+print("type of a is " + str(type(a)))
+print("type of a2 is " + str(type(a2)))
+
+a = str(a)
+
+print(a+a2)
+
+a = input("Enter new a: ")
+b = input("Enter new b: ")
+sum = int(a) + int(b)
+print(sum)
+sum2 = int(a) + float(b)
+print(sum2)
+
+a = int(a)
+b = int(b)
+
+#power
+
+print("a to the power 2 is: " + str(a**2))
+print("a to the power 3 is: " + str(a**3))
+
+#random function
+
+print((a**b) - (a*a)/(6 * 2))
