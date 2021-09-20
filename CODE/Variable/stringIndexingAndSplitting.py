666a9b64177e592e37d889330ab17dc09545d4a2
Ahmed Omar <misteradnan1@gmail.com>
2021-09-20 17:17:39 +0600

    #1 String Indexing and Splitting

---
+++ CODE/Variable/stringIndexingAndSplitting.py
@@ -0,0 +1,17 @@
+greetings = "Hi there!"
+
+print(greetings[1])
+
+print(type(greetings[1]))
+
+print(greetings[-1])
+
+print(greetings[-9])
+
+print(greetings[0:2]) #splitting in python is upper-bound exclusive
+
+print(greetings[-3:])
+
+print(greetings[-3:-1])
+
+print(greetings[3:-3])
