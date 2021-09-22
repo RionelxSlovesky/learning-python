3794ebda2b57e4e917c67b85cdf05476ddeac8c4
Ahmed Omar <misteradnan1@gmail.com>
2021-09-22 12:03:04 +0600

    #1 Dictionary

---
+++ CODE/Variable/dictionary.py
@@ -0,0 +1,20 @@
+myDictionary = {"Name": "Ahmed Omar",
+                "Age": "22",
+                "Email": "ahmed.adnan01@northsouth.edu"}
+
+print(myDictionary["Name"])
+
+print(myDictionary["Email"])
+
+print(myDictionary["Age"])
+
+print(dir(myDictionary))
+
+myDictionary = {"Name": "Ahmed Omar",
+                "Age": "22",
+                "Email": "ahmed.adnan01@northsouth.edu",
+                "PrgrammingLanguages": ["C","C++","Java","JavaScript"]}
+
+print(myDictionary["PrgrammingLanguages"])
+
+print(myDictionary["PrgrammingLanguages"][2])
