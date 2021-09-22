6ef12d8cd652c8fbea0aaec4b37337b9f98d309b
Ahmed Omar <misteradnan1@gmail.com>
2021-09-22 12:02:16 +0600

    #1 List

---
+++ CODE/Variable/lists.py
@@ -0,0 +1,13 @@
+myList = ["Hi", 2, "my", "fellow", "brothers"]
+
+print(myList[1])
+
+print(myList[0] + " " + myList[4])
+
+myList.append("worldwide")
+
+print(myList[5])
+
+myList.remove("worldwide")
+
+print(myList)
