ed82ef6ac067396cf737249758a16f2af62c5867
Ahmed Omar <misteradnan1@gmail.com>
2021-10-02 20:30:41 +0600

    #3 Functions And User Input

---
+++ CODE/Functions and Conditionals/FunctionsAndUserInput.py
@@ -0,0 +1,9 @@
+age = input("Enter your age: ")
+
+print("your age is " + age)
+
+def ageIn50Years(age):
+    newAge = int(age) + 50;
+    print("Your age in 50 years is: " + str(newAge))
+
+ageIn50Years(age)
