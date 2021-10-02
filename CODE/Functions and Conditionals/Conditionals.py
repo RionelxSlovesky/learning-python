b92d0f74d1d54fde3a58c8e7c64b12bd0eb6ca9c
Ahmed Omar <misteradnan1@gmail.com>
2021-10-02 20:51:12 +0600

    #3 Conditionals

---
+++ CODE/Functions and Conditionals/Conditionals.py
@@ -0,0 +1,9 @@
+a = input("Enter a number: ")
+a = int(a)
+
+if a<5:
+    print("The number you entered is less than 5")
+elif a==5:
+    print("The number you entered is equal to 5")
+else:
+    print("The number you entered is greater than 5")
