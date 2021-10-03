e44a244d05062eb3a4b866654e93be9a10cea5f7
Ahmed Omar <misteradnan1@gmail.com>
2021-10-03 22:35:11 +0600

    #4 For Loop

---
+++ CODE/Loops/ForLoop.py
@@ -0,0 +1,13 @@
+emails = ['jra@hotmail.com','ka@gmail.com','cya@gmail.com','rata@hotmail.com']
+
+
+print("ALL EMAILS: ")
+for i in emails:
+    print(i)
+print("\n")
+
+
+print("ONLY HOTMAILS: ")
+for i in emails:
+    if 'hotmail' in i:
+        print(i)
